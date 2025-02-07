941185bd401a7dff266f7b8ae87897692a9bcb97
olzhas-kalikhan <olzhas-kalikhan@outlook.com>
2025-02-08 01:57:55 +0500

    remove build

--- backend/dist/todos/app.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AppService, type CreateTodoPayload, type DeleteTodoPayload, type UpdateTodoPayload } from './todo.service';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    findAll(): import("./todo.service").TodoRecord[];
-    create(todoPayload: CreateTodoPayload): CreateTodoPayload;
-    update(todoPayload: UpdateTodoPayload): void;
-    remove(todoPayload: DeleteTodoPayload): number;
-}
--- backend/dist/todos/todo.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { TodoService, type CreateTodoPayload, type DeleteTodoPayload, type UpdateTodoPayload } from './todo.service';
-export declare class TodoController {
-    private readonly appService;
-    constructor(appService: TodoService);
-    findAll(): import("./todo.service").TodoRecord[];
-    create(todoPayload: CreateTodoPayload): CreateTodoPayload;
-    update(todoPayload: UpdateTodoPayload): void;
-    remove(todoPayload: DeleteTodoPayload): number;
-}
